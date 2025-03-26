342b0d8a2622e38a6abebd992ab4c7e625f748a1
Sandip Bhalodiya <bhalodia.sandip@gmail.com>
2025-03-26 17:08:39 +0530

    validation chapter

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,4 +1,4 @@
-import DashboardSkeleton from '@/app/ui/skeletons';
+// import DashboardSkeleton from '@/app/ui/skeletons';
 {/* comment this file once you implement individual component level suspense */} 
 export default function Loading() {
   // return <DashboardSkeleton />;
